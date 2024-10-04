1267ed392395ce8bb46e39957674b41aea5929e1
namunamu-to <ten1baniraSS@gmail.com>
2024-10-04 17:05:55 +0900

    websocketサーバーを立てる関数作成

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const WebSocket = require('ws');
 const https = require('https')
 var fs = require('fs');
 let connecting = true;
+let usingPorts = {};
 
 const options = {
     cert: fs.readFileSync('./fullchain.pem'),
     key: fs.readFileSync('./privkey.pem'),
 }
 
+eval(`
+   const wss = new WebSocket("wss://galleon.yachiyo.tech:34542");
+   wss.addEventListener("open", e => {
+      console.log("接続が開かれたときに呼び出されるイベント");
+   });
+`);
+
 console.log("管理画面サーバー起動");
 const manageViewServer = https.createServer(options);
 manageViewServer.on('request', (req, res) => {
     let resStr = fs.readFileSync("./index.html", 'utf-8');
+
     res.write(resStr)
     res.end()
 })
@@ -21,10 +30,12 @@ manageViewServer.listen(8445, "galleon.yachiyo.tech");
 function makeWsServer(port){
     const server = new WebSocket.Server({ port: port });
     // クライアントから接続される場合トリガーされる
+    console.log("run wsServer");
+    console.log("port : " + port);
     server.on('connection', (socket) => {
         console.log('Client connected');
+        usingPorts[port] = server;
     
-        // while (connecting) {
         // 受信メッセージを処理
         socket.on('message', (data) => {
             console.log(`Received: ${data}`);
@@ -33,10 +44,12 @@ function makeWsServer(port){
     
         // 接続中止
         socket.on('close', () => {
+            delete usingPorts[port];
             console.log('Client disconnected');
-            connecting = false;
+            console.log(usingPorts);
         });
-        // }
     });
 
 }
+
+makeWsServer(34542);
